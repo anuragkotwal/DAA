33d787f347bdb6be4b4f67c475fe6bdeed3a6ed8
Anurag Kotwal <anuragjamliya85@gmail.com>
2021-07-22 13:14:19 +0530

    Error fix

--- Week1/3_ExponentialSearch.cpp
+++ Week1/3_ExponentialSearch.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 #define endl "\n"
+
 using namespace std;
 
-void Binary_search(int* arr, int l, int h,int key){
-    int mid,comparison=0,flag=0;
+void Exponential_search(int* arr, int n, int key){
+    int comparison=0,flag=0;
+    if(arr[0] == key){
+        comparison=1;
+        cout<<"Present "<<comparison<<endl;
+        exit(0);
+    }
+    int range=1;
+    while(range<n && arr[range]<=key){
+        comparison++;
+        range *= 2;
+    }
+    int l=range/2,h=min(range,n-1),mid;
     while(h>=l){
         comparison++;
         mid=(h+l)/2;
@@ -22,24 +34,24 @@ void Binary_search(int* arr, int l, int h,int key){
 
 int main()
 {
-    int t;
     ifstream file;
-    file.open("input_2.txt");
+    file.open("input_3.txt");
     if(!file){
-        cout<<"File not found";
+        cout<<"File not found!"<<endl;
         return 0;
     }
+    int t;
     file>>t;
     while(t--){
         int n;
         file>>n;
         int arr[n];
-        for (int i = 0; i < n; i++)
+        for(int i=0; i<n; i++) 
             file>>arr[i];
         int key;
         file>>key;
-        Binary_search(arr,0,n-1,key);
-    }   
-    file.close(); 
+        Exponential_search(arr,n,key);
+    }
+    file.close();
     return 0;
 }
