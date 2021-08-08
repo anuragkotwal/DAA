a5364ef1f14d447c648b863a2c1e8fed0d429b86
Anurag Kotwal <anuragjamliya85@gmail.com>
2021-08-08 23:18:56 +0530

    Optimize program using merge sort

--- Week3/DuplicateElement.cpp
+++ Week3/DuplicateElement.cpp
@@ -8,23 +8,47 @@
 
 using namespace std;
 
-void Sort(int* arr, int n){
-    int key,i,j;
-    for(int i=1;i<n;i++){
-        key = arr[i];
-        j = i-1;
-        while(j>=0 && arr[j]>key){
-            arr[j+1] = arr[j];
-            j--;
+void merge(int* arr, int l,int m,int r){
+    int n1=m-l+1,n2=r-m;
+    int L[n1],R[n2],i,j,k;
+    for(i=0;i<n1;i++)L[i]=arr[l+i];
+    for(j=0;j<n2;j++)R[j]=arr[m+1+j];
+    i=0;j=0;k=l;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k] = L[i];
+            i++;
         }
-        arr[j+1] = key;
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]= L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]= R[j];
+        j++;
+        k++;
+    }
+}
+
+void Sort(int* arr, int l, int h){
+    if(l<h){
+        int mid = l+(h-l)/2;
+        Sort(arr,l,mid);
+        Sort(arr,mid+1,h);
+        merge(arr,l,mid,h);
     }
 }
 
 int BinarySearch(int* arr, int l, int h){
-    int mid,comparison=0,flag=1;
+    int mid,flag=1;
     while(h>=l){
-        comparison++;
         mid=(h+l)/2;
         if(arr[mid]==arr[mid+1]){
             flag=0;break;
@@ -53,7 +77,7 @@ int main()
         int arr[n];
         for (int i = 0; i < n; i++)
             file>>arr[i];
-        Sort(arr,n);
+        Sort(arr,0,n-1);
         int flag=BinarySearch(arr,0,n-1);
         if(flag)
             cout<<"NO"<<endl;
