fda3f489696dca7c7a715ca7db30ac4f3a82a0b6
Anurag Kotwal <anuragjamliya85@gmail.com>
2021-08-19 13:30:54 +0530

    Problem 3 of Week 4

---
+++ Week4/Problem_3.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+// void swap(int* arr, int i, int j){
+//     arr[i] = arr[i] + arr[j];
+//     arr[j] = arr[i] - arr[j];
+//     arr[i] = arr[i] - arr[j];
+// }
+
+int part(int* arr, int l, int h){
+    int pivot_ele = arr[h],i=l;
+    for(int j=l; j <=h ; j++){
+        if(arr[j]<pivot_ele){
+            i++;
+            swap(arr[j],arr[i]);
+        }
+    }
+    swap(arr[h],arr[i]);
+    return i;
+}
+
+int kthSmallest(int* arr, int l,int h, int k){
+    int p = part(arr,l,h);
+    if(p == k-1)
+        return arr[p];
+    else if(p<k-1)
+        return kthSmallest(arr,p+1,h,k);
+    else
+        return kthSmallest(arr,l,p-1,k);
+    return INT_MIN;
+}
+
+
+int main()
+{
+    fastio;
+    int t;
+    ifstream file;
+    file.open("input_3.txt");
+    if(!file){
+        cout<<"File not found!!";
+        return 0;
+    }
+    file>>t;
+    while(t--){
+        int n;
+        file>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            file>>arr[i];
+        int k;
+        file>>k;
+        int ans = kthSmallest(arr,0,n-1,k);
+        if(k<n)
+            cout<<ans<<endl;
+        else
+            cout<<"Not present"<<endl;
+    }
+    return 0;
+}
